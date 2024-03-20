86502a5ad19b13a759c48638ed157badeeb9c019
Nikhil2005Rai <n2005rai@gmail.com>
2024-03-20 16:09:06 +0530

    add gameRound function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const gameOpts = ["Rock", "Paper", "Scissors"]
+const gameOpts = ["rock", "paper", "scissors"]
 
 function getComputerChoice(){
     let num = Math.floor(Math.random()*3)
@@ -6,4 +6,28 @@ function getComputerChoice(){
     return gameOpts[num]
 }
 
-console.log(getComputerChoice())
+function gameRound(userChoice, computerChoice){
+    if(!gameOpts.includes(userChoice)){
+        return "Invalid input"
+
+    }
+    console.log(`You chose ${userChoice}`)
+    console.log(`Computer chose ${computerChoice}`)
+    if(userChoice === computerChoice){
+        return "It's a TIE!!!"
+    }
+    else if(userChoice === "rock" && computerChoice === "paper" ||userChoice === "paper" && computerChoice === "rock"||userChoice === "scissors" && computerChoice === "paper"){
+        return "You WON!!!"
+    }
+    else{
+        return "You LOST!!!"
+    }
+}
+
+
+
+let userChoice = prompt("Enter RPS?").toLowerCase()
+
+// console.log(getComputerChoice())
+
+console.log(gameRound(userChoice, getComputerChoice()))
